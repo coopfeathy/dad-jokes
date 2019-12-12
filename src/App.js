8287751f2cff6f37a2e2dab05fa29ba539f1553a
Cooper Featherstone <cooperfeatherstone13@gmail.com>
2019-12-11 19:28:17 -0600

    mine

--- src/App.js
+++ src/App.js
@@ -1,47 +1,26 @@
 import React, { Component } from "react"
 import "./index.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Click for a joke from dear old dad."}</button>
-        <br/>
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+import jokes from "./jokes.json"
 
 class App extends Component {
+  state = {
+   displayJoke: null
+  }
+  
+  pickJoke = () => {
+    let joke = jokes.jokes[Math.floor(Math.random() * jokes.jokes.length)];
+    console.log(joke);
+    this.setState({ displayJoke: joke });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Hey Daaaad... How'd you get in here???
-          </p>
-          <LambdaDemo />
-        </header>
+        <h1>this is my big text</h1>
+        <button onClick={this.pickJoke}>Click to do something</button>
+        <div>{this.state.displayJoke}</div>
       </div>
-    )
+    );
   }
 }
 
